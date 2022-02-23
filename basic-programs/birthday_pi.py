924c9948e5b1b65a33116d19349d06f39f12742a
aayush-commits <aayush.amoli@protonmail.ch>
2022-02-23 19:07:48 +0530

    birthday_pi

--- basic-programs/birthday_pi.py
+++ basic-programs/birthday_pi.py
@@ -7,19 +7,19 @@ from os.path import exists
 
 def check():
     bday = input("Enter you birthday (DDMMYY): ")
-    f = open('one_million_pi.txt')
-    if bday in f:
+    with open('one_million_pi.txt') as f:
+        lines = f.readlines()
+    pi_string = ''
+    for line in lines:
+        pi_string += line.rstrip()
+    if bday in pi_string:
         print("Your birthday appears in first million digits of pi value!")
     else:
         print("Your birthday does not appear in first million digits of pi value.")
-    f.close()
 
 
-if exists('one_million_pi.txt'):
+if exists('./one_million_pi.txt'):
     check()
 else:
-    res = get("https://www.piday.org/million")
-    soup = BeautifulSoup(res.text, 'html.parser')
-    million_pi = soup.find(id="million_pi").text
-    with open('one_million_pi.txt', 'w') as f:
-        f.write(million_pi)
+    print("Downloading one_million_pi.txt")
+    res = get("")
